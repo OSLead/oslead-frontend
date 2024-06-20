1fe4dfcbf3c0e277f62066f0ae219b119388e928
OSLeaderboard <osleaderboard@gmail.com>
2024-06-20 11:24:59 +0530

    Update page.tsx

--- src/app/profile/profile_maintainer/page.tsx
+++ src/app/profile/profile_maintainer/page.tsx
@@ -80,7 +80,7 @@ const Page = () => {
       <div className="max-w-4xl mx-auto p-6">
         <h1 className="text-4xl font-bold mb-6 text-center">My Profile</h1>
         {loading ? (
-          <div className="flex justify-center items-center h-full">
+          <div className="flex justify-center items-center h-screen">
             <ClipLoader color="#ffffff" loading={loading} size={50} />
           </div>
         ) : (
